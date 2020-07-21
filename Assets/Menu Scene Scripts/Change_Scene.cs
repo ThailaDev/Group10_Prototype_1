bb385d9314fec853965e7b9884e121715dc9fac1
KgosiTheShogun <64078943+KgosiTheShogun@users.noreply.github.com>
2020-07-21 15:15:53 +0200

    commit

---
+++ Assets/Menu Scene Scripts/Change_Scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_Scene : MonoBehaviour
+{
+    public int _SceneNumber;
+
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + _SceneNumber);
+    }
+}
