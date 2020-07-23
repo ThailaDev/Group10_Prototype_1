c47f5c2abef7bfae5712726f257ab5987b22524c
KgosiTheShogun <64078943+KgosiTheShogun@users.noreply.github.com>
2020-07-23 15:32:50 +0200

    Menu Custom Buttons & Animations Added

---
+++ Assets/UI/Scripts/QuitButton_ControlScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class QuitButton_ControlScript : MonoBehaviour
+{
+    public Animator QuitButton;
+    public AudioSource ButtonSound;
+    
+    public float ClosingGameTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonSound.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ButtonSelected()
+    {
+        QuitButton.SetBool("Selected", true);
+    }
+
+    public void ButtonDeselected()
+    {
+        QuitButton.SetBool("Selected", false);
+    }
+
+    public void ButtonPressed()
+    {
+        QuitButton.SetBool("Pressed", true);
+        ButtonSound.Play();
+        StartCoroutine("ChangeScene");
+    }
+
+
+
+    IEnumerator ChangeScene()
+    {
+        QuitButton.SetBool("Pressed", false);
+
+        ButtonSound.Stop();
+
+        Application.Quit();
+
+        yield return new WaitForSeconds(ClosingGameTime);
+
+        
+    }
+
+}
+
